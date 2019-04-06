2b0125680c7839bc9c2c2bdc7c0d010d6d7aada6
AntiAegis <thuypn9a4@gmail.com>
2019-04-06 17:34:38 +0700

    Add custom_pspnet_miou

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -6,24 +6,7 @@ from torch.nn import functional as F
 
 
 #------------------------------------------------------------------------------
-#   IoU with sigmoid
-#------------------------------------------------------------------------------
-def iou_with_sigmoid(sigmoid, targets, eps=1e-6):
-	"""
-	sigmoid: (torch.float32) shape (N, 1, H, W)
-	targets: (torch.float32) shape (N, H, W), value {0,1}
-	"""
-	outputs = torch.squeeze(sigmoid, dim=1).type(torch.int8)
-	targets = targets.type(torch.int8)
-
-	inter = (outputs & targets).type(torch.float32).sum(dim=(1,2))
-	union = (outputs | targets).type(torch.float32).sum(dim=(1,2))
-	iou = inter / (union + eps)
-	return iou.mean()
-
-
-#------------------------------------------------------------------------------
-#   mIoU
+#   Fundamental metrics
 #------------------------------------------------------------------------------
 def miou(logits, targets, eps=1e-6):
 	"""
@@ -41,12 +24,40 @@ def miou(logits, targets, eps=1e-6):
 	return iou.mean()
 
 
+def iou_with_sigmoid(sigmoid, targets, eps=1e-6):
+	"""
+	sigmoid: (torch.float32) shape (N, 1, H, W)
+	targets: (torch.float32) shape (N, H, W), value {0,1}
+	"""
+	outputs = torch.squeeze(sigmoid, dim=1).type(torch.int8)
+	targets = targets.type(torch.int8)
+
+	inter = (outputs & targets).type(torch.float32).sum(dim=(1,2))
+	union = (outputs | targets).type(torch.float32).sum(dim=(1,2))
+	iou = inter / (union + eps)
+	return iou.mean()
+
+
 #------------------------------------------------------------------------------
 #   Custom IoU for BiSeNet
 #------------------------------------------------------------------------------
 def custom_bisenet_miou(logits, targets):
 	"""
 	logits: (torch.float32) (main_out, feat_os16_sup, feat_os32_sup) of shape (N, C, H, W)
+	targets: (torch.float32) shape (N, H, W), value {0,1,...,C-1}
+	"""
+	if type(logits)==tuple:
+		return miou(logits[0], targets)
+	else:
+		return miou(logits, targets)
+
+
+#------------------------------------------------------------------------------
+#   Custom IoU for PSPNet
+#------------------------------------------------------------------------------
+def custom_pspnet_miou(logits, targets):
+	"""
+	logits: (torch.float32) (main_out, aux_out) of shape (N, C, H, W), (N, C, H/8, W/8)
 	targets: (torch.float32) shape (N, H, W), value {0,1,...,C-1}
 	"""
 	if type(logits)==tuple:
